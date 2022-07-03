c517db2dee612716b257c4f8a27c55e23dc445c2
Steven Sidhu <steve.sidhu@hotmail.com>
2022-07-03 01:37:15 -0700

    useRef demo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
+import { useRef, useState } from "react";
 import "./App.css";
 import useRandomJoke from "./useRandomJoke";
 
 function App() {
-  const joke = useRandomJoke();
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+
+  const [firstName, setFirstName] = useState("Steven");
+  const [lastName, setLastName] = useState("Sidhu");
+
+  const joke = useRandomJoke(firstName, lastName);
+
+  const generateJoke = (e) => {
+    e.preventDefault();
+    setFirstName(firstNameRef.current.value);
+    setLastName(lastNameRef.current.value);
+  };
 
   return (
     <div className="App">
       <center>
         <h1>Joke Generator</h1>
-        <button>Generate Joke</button>
 
+        <form>
+          <input placeholder="first name" ref={firstNameRef} />
+          <input placeholder="last name" ref={lastNameRef} />
+          <button onClick={generateJoke}>Generate Joke</button>
+        </form>
         <h2>{joke}</h2>
       </center>
     </div>
--- src/useRandomJoke.js
+++ src/useRandomJoke.js
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 
-function useRandomJoke() {
+function useRandomJoke(firstName, lastName) {
   const [joke, setJoke] = useState("");
 
   useEffect(() => {
     const fetchJoke = async () =>
       await fetch(
-        `http://api.icndb.com/jokes/random?firstName=steven&lastName=sidhu`
+        `http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}`
       )
         .then((res) => res.json())
         .then((data) => {
@@ -14,7 +14,7 @@ function useRandomJoke() {
         });
 
     fetchJoke();
-  }, []);
+  }, [firstName, lastName]);
 
   return joke;
 }
