c0ea502c889fa79fa3f50d76b5144b090e96ff38
Steven Sidhu <steve.sidhu@hotmail.com>
2022-07-03 01:02:26 -0700

    generate random joke from fetch api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [joke, setJoke] = useState("");
+
+  useEffect(() => {
+    const fetchJoke = async () =>
+      await fetch(
+        `http://api.icndb.com/jokes/random?firstName=steven&lastName=sidhu`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setJoke(data.value.joke);
+        });
+
+    fetchJoke();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <h1>Joke Generator</h1>
+        <button>Generate Joke</button>
+
+        <h2>{joke}</h2>
+      </center>
     </div>
   );
 }
